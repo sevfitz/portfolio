57d45df417753c14af9e945a9640c723a5f742e2
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-06-06 10:34:18 -0700

    Fix linter errors

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ var siteView = {};
 siteView.navHandler = function() {
     $('nav li.tab').on('click', function () {
         $('.tab-content').hide();
-        var clickedTab = $(this).data('content');
+        let clickedTab = $(this).data('content');
         $('#' + clickedTab).show();
     });
     $('nav .tab:first').click();
