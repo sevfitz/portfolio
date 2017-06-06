801c3c6435096e93ab7182918ebd250d40ffa000
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-06-05 17:45:17 -0700

    Fixed img paths to show in live-server

--- js/portfolioData.js
+++ js/portfolioData.js
@@ -7,7 +7,7 @@ var portfolioItemData = [
 		dev: 'Stephanie Fitzgerald',
 		repoURL: 'https://github.com/sevfitz/cookie-stand',
 		ghPagesURL: 'https://sevfitz.github.io/cookie-stand',
-		img: '/Users/fitzgerald/Documents/CodeFellows/301/labs/portfolio/images/cookie-stand.png',
+		img: 'images/cookie-stand.png',
 		lastUpdated: '2017-05-18',
 		isTeamProj: false,
 		description: '<p>Marketing and sales data pages for fictitious cookie store franchise</p>'
@@ -18,7 +18,7 @@ var portfolioItemData = [
 		dev: 'Stephanie Fitzgerald',
 		repoURL: 'https://github.com/sevfitz/busmall',
 		ghPagesURL: 'https://sevfitz.github.io/busmall',
-		img: '/Users/fitzgerald/Documents/CodeFellows/301/labs/portfolio/images/busmall.png',
+		img: 'images/busmall.png',
 		lastUpdated: '2017-05-18',
 		isTeamProj: false,
 		description: '<p>Fictitious survey research site that displays data with Chart.js</p>'
@@ -29,7 +29,7 @@ var portfolioItemData = [
 		dev: 'Team Chums',
 		repoURL: 'https://sevfitz.github.io/turducken',
 		ghPagesURL: 'https://sevfitz.github.io/turducken',
-		img: '/Users/fitzgerald/Documents/CodeFellows/301/labs/portfolio/images/turducken.png',
+		img: 'images/turducken.png',
 		lastUpdated: '2017-05-27',
 		isTeamProj: true,
 		description: '<p>Final group project for Code 201: prototype for consumer-level social media consolidator site</p>'
