36355832960ae845e05c274c0ef1b571375d3789
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-06-12 16:48:18 -0700

    Wrap PortfolioItem in IIFE

--- public/js/portfolioItem.js
+++ public/js/portfolioItem.js
@@ -1,76 +1,81 @@
 'use strict';
 
-function PortfolioItem(portfolioItemObj) {
-	this.project = portfolioItemObj.project;
-	this.id = portfolioItemObj.id;
-	this.dev = portfolioItemObj.dev;
-	this.repoURL = portfolioItemObj.repoURL;
-	this.ghPagesURL = portfolioItemObj.ghPagesURL;
-	this.img = portfolioItemObj.img;
-	this.lastUpdated = portfolioItemObj.lastUpdated;
-	this.isTeamProj = portfolioItemObj.isTeamProj;
-	this.description = portfolioItemObj.description;
-}
+var app = app || {};  //eslint-disable-line
 
-PortfolioItem.all = [];
+(function (module) {                                    //eslint-disable-line
+	function PortfolioItem(portfolioItemObj) {
+		this.project = portfolioItemObj.project;
+		this.id = portfolioItemObj.id;
+		this.dev = portfolioItemObj.dev;
+		this.repoURL = portfolioItemObj.repoURL;
+		this.ghPagesURL = portfolioItemObj.ghPagesURL;
+		this.img = portfolioItemObj.img;
+		this.lastUpdated = portfolioItemObj.lastUpdated;
+		this.isTeamProj = portfolioItemObj.isTeamProj;
+		this.description = portfolioItemObj.description;
+	}
 
-PortfolioItem.prototype.toHtml = function () {
-	let template = Handlebars.compile($('#template').text());
+	PortfolioItem.all = [];
 
-	this.daysAgo = parseInt((new Date() - new Date(this.lastUpdated)) / 60 / 60 / 24 / 1000);
+	PortfolioItem.prototype.toHtml = function () {
+		let template = Handlebars.compile($('#template').text());  //eslint-disable-line
 
-	return template(this);
-};
+		this.daysAgo = parseInt((new Date() - new Date(this.lastUpdated)) / 60 / 60 / 24 / 1000);
 
-PortfolioItem.loadAll = function (pfItem) {
-	pfItem.sort(function (a, b) {
-		return (new Date(b.lastUpdated)) - (new Date(a.lastUpdated));
-	});
+		return template(this);
+	};
 
-	pfItem.forEach(function (item) {
-		PortfolioItem.all.push(new PortfolioItem(item));
-	});
-}
+	PortfolioItem.loadAll = function (pfItem) {
+		pfItem.sort(function (a, b) {
+			return (new Date(b.lastUpdated)) - (new Date(a.lastUpdated));  //eslint-disable-line
+		});
 
-PortfolioItem.fetchAll = function () {
-	if (localStorage.portfolioItems) {
+		pfItem.forEach(function (item) {
+			PortfolioItem.all.push(new PortfolioItem(item));
+		});
+	}
+
+	PortfolioItem.fetchAll = function () {
+		if (localStorage.portfolioItems) {
+			$.ajax({
+				type: 'HEAD',
+				url: './index.html',
+				success: PortfolioItem.eTagCheck,
+				error: PortfolioItem.runWhenFails
+			});
+		} else {
+			PortfolioItem.getPFData();
+		}
+	};
+
+	PortfolioItem.getPFData = function () {
 		$.ajax({
-			type: 'HEAD',
-			url: './index.html',
-			success: PortfolioItem.eTagCheck,
+			type: 'GET',
+			url: './data/portfolioData.json',
+			success: PortfolioItem.runWhenDone,
 			error: PortfolioItem.runWhenFails
 		});
-	} else {
-		PortfolioItem.getPFData();
-	}
-};
+	};
 
-PortfolioItem.getPFData = function () {
-	$.ajax({
-		type: 'GET',
-		url: './data/portfolioData.json',
-		success: PortfolioItem.runWhenDone,
-		error: PortfolioItem.runWhenFails
-	});
-};
+	PortfolioItem.runWhenDone = function (pfData, message, res) {
+		PortfolioItem.loadAll(pfData);
+		localStorage.setItem('portfolioItems', JSON.stringify(pfData));
+		localStorage.setItem('eTag', res.getResponseHeader('eTag'));
+		siteView.initIndexPage();  //eslint-disable-line
+	};
 
-PortfolioItem.runWhenDone = function (pfData, message, res) {
-	PortfolioItem.loadAll(pfData);
-	localStorage.setItem('portfolioItems', JSON.stringify(pfData));
-	localStorage.setItem('eTag', res.getResponseHeader('eTag'));
-	siteView.initIndexPage();
-};
+	PortfolioItem.runWhenFails = function (err) {
+		console.log('error: ', err);
+	};
 
-PortfolioItem.runWhenFails = function (err) {
-	console.log('error: ', err);
-};
-
-PortfolioItem.eTagCheck = function (data, message, res) {
-	let eTag = res.getResponseHeader('eTag');
-	let storedeTag = localStorage.getItem('eTag');
-	if (eTag === storedeTag) {
-		PortfolioItem.runWhenDone();
-	} else {
-		PortfolioItem.getPFData();
-	}
-};
+	PortfolioItem.eTagCheck = function (data, message, res) {
+		let eTag = res.getResponseHeader('eTag');
+		let storedeTag = localStorage.getItem('eTag');
+		if (eTag === storedeTag) {
+			PortfolioItem.runWhenDone();
+		} else {
+			PortfolioItem.getPFData();
+		}
+	};
+	module.PortfolioItem = PortfolioItem;
+})(app);
